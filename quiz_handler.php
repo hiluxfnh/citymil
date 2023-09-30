0df31e27240f65688e71bbc0aed9a1ec90659716
Hilux FOKOU <hiluxfokou@gmail.com>
2023-09-30 18:07:48 +0530

    New pages

--- quiz_handler.php
+++ quiz_handler.php
@@ -1,64 +1,46 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link
-      rel="stylesheet"
-      href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
-      integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
-      crossorigin="anonymous"
-    />
-    <script
-      src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
-      integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
-      crossorigin="anonymous"
-    ></script>
-    <link
-      rel="stylesheet"
-      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
-    />
-    <link rel="stylesheet" href="css/style.css" />
-    <title>Welcome | CityMIL</title>
-    <link rel="icon" href="img/logo.png" type="image/x-icon" />
-    <link rel="apple-touch-icon" sizes="180x180" href="img/logo.png" />
-  </head>
-  <body>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <a class="navbar-brand" href="index.html">CityMIL</a>
-      <button
-        class="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarNav"
-        aria-controls="navbarNav"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
-          <li class="nav-item">
-            <a class="nav-link" href="#" target="_blank">Topics</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Play Ground</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">About Us</a>
-          </li>
-        </ul>
-      </div>
-    </nav>
 
-    <img class="d-block w-100" src="img/m2.png" alt="Second slide" />
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous" />
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
+  <link rel="stylesheet" href="css/style.css" />
+  <title>Welcome | CityMIL</title>
+  <link rel="icon" href="img/logo.png" type="image/x-icon" />
+  <link rel="apple-touch-icon" sizes="180x180" href="img/logo.png" />
+</head>
 
-<!-- Beautifying the result page and adding a badge on with user score -->
-<?php
-// Define the correct answers for each question
-$correctAnswers = array(
+<body>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="index.html">CityMIL</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" href="index.html#topics">Topics</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="playground.html">Play Ground</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">About Us</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+
+  <img class="d-block w-100" src="img/a1.png"/>
+
+  <!-- Beautifying the result page and adding a badge on with user score -->
+  <?php
+  // Define the correct answers for each question
+  $correctAnswers = array(
     'q1' => 'a',
     'q2' => 'c',
     'q3' => 'c',
@@ -69,126 +51,125 @@ $correctAnswers = array(
     'q8' => 'b',
     'q9' => 'c',
     'q10' => 'b',
-);
+  );
 
-// Initialize the score
-$score = 0;
+  // Initialize the score
+  $score = 0;
 
-// Loop through each question and check if the submitted answer is correct
-foreach ($correctAnswers as $question => $correctAnswer) {
+  // Loop through each question and check if the submitted answer is correct
+  foreach ($correctAnswers as $question => $correctAnswer) {
     if (isset($_POST[$question]) && $_POST[$question] === $correctAnswer) {
-        $score++;
+      $score++;
     }
-}
-?>
+  }
+  ?>
 
-    <div class="container">
-      <div class="row">
-        <div class="col-md-12">
-          <div class="card mt-5">
-            <div class="card-header">
-              <h4>Result</h4>
-            </div>
-            <div class="card-body">
-              <h1 class="text-center"><?php if($score>=5) {echo "Congratulations! 🎉";} else {echo "Sorry!";} ?></h1>
-              <h2 class="text-center">You have <?php if($score>=8) {echo "PASSED";} else {echo "FAILED";} ?> the quiz</h2>
-              <h3 class="text-center">Your Score is: <?php echo $score; ?>/10</h3>
-              <!-- You won a badge -->
-                <?php if($score>=8) {echo "<center><img src='img/media-badge.png' alt='badge' width='150px' height='150px'></center>";} ?>
-              <br>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <div class="card mt-5">
+          <div class="card-header">
+            <h4>Result</h4>
+          </div>
+          <div class="card-body">
+            <h1 class="text-center"><?php if ($score >= 7) {
+                                      echo "Congratulations! 🎉";
+                                    } else {
+                                      echo "Sorry!";
+                                    } ?></h1>
+            <h2 class="text-center">You have <?php if ($score >= 7) {
+                                                echo "PASSED";
+                                              } else {
+                                                echo "FAILED";
+                                              } ?> the quiz</h2>
+            <h3 class="text-center">Your Score is: <?php echo $score; ?>/10</h3>
+            <!-- You won a badge -->
+            <?php if ($score >= 7) {
+              echo "<hr><center>You have won a badge!</center>";
+              echo "<center><img src='img/media-badge.png' alt='badge' width='150px' height='150px'></center>";
+            }
+          else{
+            echo "<hr><center>Atleast <b>70%</b> to Pass :(</center>";
+          } ?>
+            <br>
 
-              <center><?php if($score>=5){} ?><a href="quiz-media.html" class="btn btn-primary">Take Again</a></center>
-            </div>
+            <?php
+            if ($score >= 7) {
+              // If the score is greater than or equal to 7, go to the next page
+              echo '<center><a href="quiz-media.html" class="btn btn-primary">Take Again</a> <a href="index.html#topics" class="btn btn-primary">Go to a new Topic</a></center>';
+            } else {
+              // If the score is less than 7, allow the user to try the quiz again
+              echo '<center><a href="quiz-media.html" class="btn btn-primary">Try Again</a></center>';
+            }
+            ?>
+
           </div>
         </div>
       </div>
     </div>
+  </div>
 
-    <!-- Footer starts Here -->
-    <footer class="footer-distributed">
-      <div class="footer-left">
-        <h3>City<span>MIL</span></h3>
-        <br />
-        <p class="footer-links">
-          <a href="#">Home</a>
-          ·
-          <a href="#">Topics</a>
-          ·
-          <a href="#">Play Ground</a>
-          ·
-          <a href="#">About Us</a>
-        </p>
-        <p class="footer-company-name">CityMIL &copy; 2023</p>
+  <!-- Footer starts Here -->
+  <footer class="footer-distributed">
+    <div class="footer-left">
+      <h3>City<span>MIL</span></h3>
+      <br />
+      <p class="footer-links">
+        <a href="#">Home</a>
+        ·
+        <a href="#">Topics</a>
+        ·
+        <a href="#">Play Ground</a>
+        ·
+        <a href="#">About Us</a>
+      </p>
+      <p class="footer-company-name">CityMIL &copy; 2023</p>
+    </div>
+    <div class="footer-center">
+      <div>
+        <i class="fa fa-map-marker"></i>
+        <span>CityMIL</span> Bangalore, India <br /><br />
       </div>
-      <div class="footer-center">
-        <div>
-          <i class="fa fa-map-marker"></i>
-          <span>CityMIL</span> Bangalore, India <br /><br />
-        </div>
-        <div>
-          <i class="fa fa-phone"></i>
-          (+91) 9108553983<br /><br />
-        </div>
-        <div>
-          <i class="fa fa-envelope"></i>
-          <a href="mailto:hiluxfokou33@gmail.com"></a>hiluxfokou33@gmail.com
-        </div>
+      <div>
+        <i class="fa fa-phone"></i>
+        (+91) 9108553983<br /><br />
       </div>
-      <div class="footer-right">
-        <p class="footer-company-about">
-          <span>About CityMIL</span>
-          CityMIL is a platform for Media and Information Literacy (MIL) and
-          Cyber Security Awareness (CSA) education. It is a joint initiative
-          between 5 young people from Bangladesh, Cameroon, India, Tanzania, and
-          Togo and UNESCO MGIEP.
-        </p>
+      <div>
+        <i class="fa fa-envelope"></i>
+        <a href="mailto:hiluxfokou33@gmail.com"></a>hiluxfokou33@gmail.com
+      </div>
+    </div>
+    <div class="footer-right">
+      <p class="footer-company-about">
+        <span>About CityMIL</span>
+        CityMIL is a platform for Media and Information Literacy (MIL) and
+        Cyber Security Awareness (CSA) education. It is a joint initiative
+        between 5 young people from Bangladesh, Cameroon, India, Tanzania, and
+        Togo and UNESCO MGIEP.
+      </p>
 
-        <div class="footer-icons">
-          <a href="https://www.facebook.com/citymilnepal" target="_blank"
-            ><i class="fab fa-facebook-f"></i
-          ></a>
-          <a href="https://twitter.com/citymilnepal" target="_blank"
-            ><i class="fab fa-twitter"></i
-          ></a>
-          <a href="https://www.instagram.com/citymilnepal/" target="_blank"
-            ><i class="fab fa-instagram"></i
-          ></a>
-          <a
-            href="https://www.youtube.com/channel/UCZ3Z6Z6Z2ZqQ4Z9XZ9Z9Z9Q"
-            target="_blank"
-            ><i class="fab fa-youtube"></i
-          ></a>
-        </div>
-        <form>
-          <select>
-            <option>English</option>
-            <option>Français</option>
-            <option>Swahili</option>
-            <option>हिन्दी</option>
-            <option>বাংলা</option>
-          </select>
-        </form>
+      <div class="footer-icons">
+        <a href="https://www.facebook.com/citymilnepal" target="_blank"><i class="fab fa-facebook-f"></i></a>
+        <a href="https://twitter.com/citymilnepal" target="_blank"><i class="fab fa-twitter"></i></a>
+        <a href="https://www.instagram.com/citymilnepal/" target="_blank"><i class="fab fa-instagram"></i></a>
+        <a href="https://www.youtube.com/channel/UCZ3Z6Z6Z2ZqQ4Z9XZ9Z9Z9Q" target="_blank"><i class="fab fa-youtube"></i></a>
       </div>
-    </footer>
+      <form>
+        <select>
+          <option>English</option>
+          <option>Français</option>
+          <option>Swahili</option>
+          <option>हिन्दी</option>
+          <option>বাংলা</option>
+        </select>
+      </form>
+    </div>
+  </footer>
 
-    <!-- Bootstrap core JavaScript for the cross-->
-    <script
-      src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
-      integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
-      crossorigin="anonymous"
-    ></script>
-    <script
-      src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"
-      integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN"
-      crossorigin="anonymous"
-    ></script>
-    <script
-      src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js"
-      integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s"
-      crossorigin="anonymous"
-    ></script>
-  </body>
+  <!-- Bootstrap core JavaScript for the cross-->
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
+</body>
+
 </html>
-
-
-
